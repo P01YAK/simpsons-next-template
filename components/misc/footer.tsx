0267d3cd72fd636293ba054cd06d137fa12c316d
POLYAK <legin.danya@gmail.com>
2023-11-22 23:22:24 +0300

    Update

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 			<div className='container mx-auto px-5'>
 				<div className='py-20 flex flex-col lg:flex-row items-center'>
 					<h3 className='text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2'>
-						<PreviewLink href='/seasons/seasons'>Можешь читать</PreviewLink>, а
+						<PreviewLink href='/seasons'>Можешь читать</PreviewLink>, а
 						можешь и нет.
 					</h3>
 					<div className='flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2'>
